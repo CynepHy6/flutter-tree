02727c9e4228ecb292d9aafe762624f1b8bc34ac
CynepHy6 <CynepHy6@gmail.com>
2022-01-14 21:20:58 +0300

    add test

---
+++ tests/src/core/text-index.ts
@@ -0,0 +1,10 @@
+import * as vscode from 'vscode';
+import { expect } from 'chai';
+
+
+describe('index', () => {
+    it('test work', async function () {
+        // const [result] = await Promise.all([buildResponse(payload)]);
+        expect(1).equal(1);
+    });
+});
---
+++ tests/src/text-completion.ts
@@ -0,0 +1,10 @@
+import * as vscode from 'vscode';
+import * as chai from 'chai';
+
+
+describe('completion', () => {
+    it('test work', async function () {
+        // const [result] = await Promise.all([buildResponse(payload)]);
+        chai.expect(1).equal(1);
+    });
+});
